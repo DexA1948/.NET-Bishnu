3dcb19704f136d018240c03f790bdf255c0e9291
Bishnu Rawal <rawal.bishnu@gmail.com>
2022-04-21 11:57:14 +0545

    Discuss on loops

--- Classes/CSharpFundamentals/EntryPoint.cs
+++ Classes/CSharpFundamentals/EntryPoint.cs
@@ -10,5 +10,25 @@ public class Program
         var d2 = calculator1.Subtract();
         var m2 = calculator1.Multiply();
         var di2 = calculator1.Divide();
+
+
+        for (long i = 10; i < 20; i++)
+        {
+            Console.WriteLine("Hi");
+        }
+
+        double[] weights = { 34.5, 67.8, 23.5, 34.1 };
+        var t = weights[4];
+
+        for(byte j = 0; j < weights.Length; j++)
+        {
+            Console.WriteLine(weights[j]);
+        }
+
+        foreach(var x in weights)
+        {
+            Console.WriteLine(x);
+        }
+
     }
 }
