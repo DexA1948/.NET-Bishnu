5048537added4ea62792f4860b4f394a1b3238f7
Bishnu Rawal <rawal.bishnu@gmail.com>
2022-05-05 11:14:02 +0545

    Discuss properties

--- Classes/CSharpFundamentals/EntryPoint.cs
+++ Classes/CSharpFundamentals/EntryPoint.cs
@@ -1,34 +1,16 @@
-using ABC;
 public class Program
 {
     public static void Main()
     {
-        BasicCalculator calculator1 = new(34.6, 35.6);
-        var y = BasicCalculator.brand;
+        Person p1 = new();
+        p1.Name = "c";
 
-        var s2 = calculator1.Sum();
-        var d2 = calculator1.Subtract();
-        var m2 = calculator1.Multiply();
-        var di2 = calculator1.Divide();
+        var x = p1.Name;
+        var y = p1.Address;
 
-
-        for (long i = 10; i < 20; i++)
-        {
-            Console.WriteLine("Hi");
-        }
-
-        double[] weights = { 34.5, 67.8, 23.5, 34.1 };
-        var t = weights[4];
-
-        for(byte j = 0; j < weights.Length; j++)
-        {
-            Console.WriteLine(weights[j]);
-        }
-
-        foreach(var x in weights)
-        {
-            Console.WriteLine(x);
-        }
-
+        //Defining constants
+        const int f = 445;
+        readonly byte g = 45;
+        
     }
 }
