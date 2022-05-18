d99a36923e98c0d4a83835437337baef911453b1
Bishnu Rawal <rawal.bishnu@gmail.com>
2022-05-18 11:50:16 +0545

    Edit on exception

--- Classes/CSharpFundamentals/ExceptionHandling.cs
+++ Classes/CSharpFundamentals/ExceptionHandling.cs
@@ -3,10 +3,13 @@ using System;
 public class ExceptionHandling
 {
     public double Compute(double x, double y, double z)
-    {
+    {   
         double c = 0;
         try 
         {
+            if( x == 0 || y == 0 )
+                throw new Exception("Parameter value 0 supplied on x or y");
+
             double value = (x + y) / z;
             c = value * x / (y + z);
         }
@@ -30,7 +33,8 @@ public class ExceptionHandling
 
 
     public void Test()
-    {
+    {        
         var v = Compute(23, 45, 0);
+        var v1 = Compute(0, 0, 0);
     }
 }
