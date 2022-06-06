0a42522583976d80b87bd2e34513b473d1338029
Bishnu Rawal <rawal.bishnu@gmail.com>
2022-06-06 11:57:01 +0545

    Bind college list to view

--- Classes/WebDev/AssigmentsManagement/Controllers/CollegeController.cs
+++ Classes/WebDev/AssigmentsManagement/Controllers/CollegeController.cs
@@ -1,9 +1,19 @@
+using AssigmentsManagement.Models;
 using Microsoft.AspNetCore.Mvc;
 
 public class CollegeController: Controller
 {
+    List<College> colleges = new()
+    {
+        new College() { Name = "Vedas", Description = "IT college, affiliated to TU", Address = "Lalitpur", EstablishedDate = DateTime.Now, Rank = 7 },
+        new College() { Name = "Himalayan", Description = "IT college, affiliated to TU", Address = "Bhaktapur", EstablishedDate = DateTime.Now, Rank = 3 },
+        new College() { Name = "Vedas1", Description = "IT college, affiliated to TU", Address = "Lalitpur", EstablishedDate = DateTime.Now, Rank = 1 },
+        new College() { Name = "Vedas2", Description = "IT college, affiliated to TU", Address = "Bhaktapur", EstablishedDate = DateTime.Now, Rank = 4 },
+        new College() { Name = "Vedas3", Description = "IT college, affiliated to TU", Address = "Kathmandu", EstablishedDate = DateTime.Now, Rank = 10 }
+    };
+
     public IActionResult Index()
     {
-        return View();
+        return View(colleges);
     }
 }
